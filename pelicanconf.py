dc7cfe33e8aa5bb931387fa7da4347e7a92c0766
Allen Barker <Allen.L.Barker@gmail.com>
2017-08-15 23:47:46 -0400

    comments

--- pelicanconf.py
+++ pelicanconf.py
@@ -15,7 +15,7 @@ SITEURL = 'https://abarker.github.io' # Doesn't seem to do much, needed for Disq
 GITHUB_URL = 'https://github.com/abarker' # may not do anything with theme, some make fork ribbon
 
 DISQUS_SITENAME = 'abarker' # Should be the shortname.
-DISQUS_URL = 'https://abarker.disqus.com' # NOT USED FOR ELEGANT THEME, some themes use...
+##DISQUS_URL = 'https://abarker.disqus.com' # NOT USED FOR ELEGANT THEME, some themes use...
 ##DISQUS_SECRET_KEY = u'YOUR_SECRET_KEY' # only for disqus_static plugin, may not need
 ##DISQUS_PUBLIC_KEY = u'YOUR_PUBLIC_KEY' # only for disqus_static plugin, may not need
 
